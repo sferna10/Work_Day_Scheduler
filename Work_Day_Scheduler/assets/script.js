09f76007e8a5e3b5edd9a25ba6578a4b4bfe5802
sferna10 <sferna10@email.cpcc.edu>
2023-10-02 23:47:59 -0400

    fixing bugs

--- Work_Day_Scheduler/assets/script.js
+++ Work_Day_Scheduler/assets/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
-$(',saveBtn').on('click', function(){
+$('.saveBtn').on('click', function(){
 })
 var value = $(this).siblings('.description').val();
 var time = $(this).parent().attr('id');
